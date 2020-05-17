3e984c3538b23b17b485899e0e8489c00f2d6524
minhitmta2016 <congminh.it.mta@gmail.com>
2020-05-17 22:36:01 +0700

    Xoa File bak

---
+++ BookStore/Controllers/DetailProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class DetailProductController : Controller
+    {
+        // GET: DetailProduct
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
