44ff9c2b0807dac40276315f13ce392f6852cfaa
minhitmta2016 <congminh.it.mta@gmail.com>
2020-05-17 20:54:00 +0700

    Cập nhật sách theo chủ đề

--- BookStore/Controllers/BooksController.cs
+++ BookStore/Controllers/BooksController.cs
@@ -11,6 +11,7 @@ namespace BookStore.Controllers
     public class BooksController : Controller
     {
         private Model1 context= new Model1();
+        List<Sach> sach = new List<Sach>();
         private List<Chude> GetChudes()
         {
             return context.Chudes.Where(i => i.Tenchude != null).ToList();
@@ -20,17 +21,39 @@ namespace BookStore.Controllers
             return context.Saches.ToList();
         }
         // GET: Books
-        public ActionResult Index()
+        public ActionResult Index(int id=1)
         {
-            ViewBag.ListAllBooks = GetAllBooks();
-            ViewBag.ListBookCate = GetChudes();
+            //ViewBag.ListAllBooks = GetAllBooks();
+            //ViewBag.ListBookCate = GetChudes();
+            //return View();
+            
+            if (id == 1)
+            {
+                sach = context.Saches.ToList();
+            }
+            else
+            {
+                sach = context.Saches.Where(i => i.Macd == id).ToList();
+            }        
+            return View(sach);
+
+
+        }
+        public ActionResult GetProductTag()
+        {
+
+            var chude = from cd in context.Chudes select cd;
+            return PartialView(chude);
+        }
+        public ActionResult SanPhamTheoChuDe(int id)
+        {
+            var sach = context.Saches.Where(i => i.Macd == id);
             return View();
 
         }
 
-        /// tesst
-//test 2
-//test 3
+
+  
 
     }
 }
