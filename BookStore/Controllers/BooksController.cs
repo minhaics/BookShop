8830511a5ad73eedbb04ac371e8ad1bb5bf0aa43
minhitmta2016 <congminh.it.mta@gmail.com>
2020-05-17 12:31:59 +0700

    Add project files.

---
+++ BookStore/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        private Model1 context= new Model1();
+        private List<Chude> GetChudes()
+        {
+            return context.Chudes.Where(i => i.Tenchude != null).ToList();
+        }
+        private List<Sach> GetAllBooks()
+        {
+            return context.Saches.ToList();
+        }
+        // GET: Books
+        public ActionResult Index()
+        {
+            ViewBag.ListAllBooks = GetAllBooks();
+            ViewBag.ListBookCate = GetChudes();
+            return View();
+
+        }
+
+    }
+}
---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using BookStore.Models;
+namespace BookStore.Controllers
+
+{
+    public class HomeController : Controller
+    {
+        private Model1 context = new Model1();
+        private List<Sach> GetNewBooks(int count)
+        {
+            return context.Saches.OrderByDescending(i => i.Ngaycapnhat).Take(count).ToList();
+        }
+        private List<Chude> GetChudes()
+        {
+            return context.Chudes.Where(i => i.Tenchude != null).ToList();
+        }
+
+        private List<Sach> GetAllBooks()
+        {
+            return context.Saches.ToList();
+        }
+        public ActionResult Index()
+        {
+
+            ViewBag.ListBookCate = GetChudes();
+            ViewBag.ListAllBooks = GetAllBooks();
+            var newbooks = GetNewBooks(5);
+                return View(newbooks);
+        }
+
+        
+        
+    }
+}
