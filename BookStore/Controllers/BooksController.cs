93870dbb157e34e98ed1ceeb039851bd579d276f
vanphong2809 <vanphong6822@gmail.com>
2020-05-21 18:12:49 +0700

    Hoàn thiện hơn trang Home, Books, Details

--- BookStore/Controllers/BooksController.cs
+++ BookStore/Controllers/BooksController.cs
@@ -14,44 +14,86 @@ namespace BookStore.Controllers
         List<Sach> sach = new List<Sach>();
         private List<Chude> GetChudes()
         {
+            
             return context.Chudes.Where(i => i.Tenchude != null).ToList();
         }
         private List<Sach> GetAllBooks()
         {
             return context.Saches.ToList();
         }
+        public List<Sach> GetBookByChude(int? id)
+        {
+            return context.Saches.Where(i => i.Macd == id).ToList();
+        }
+        //public int CountBookByChuDe(int? id)
+        //{
+        //    List<Sach> items = context.Saches.ToList();
+        //    //var count = items.Where(i => i.Macd == id).Count();
+        //    var count = from s in items
+        //                group s by s.Macd into sachbychude
+        //                select new
+        //                {
+        //                    Macd = sachbychude.Key,
+        //                    Count = sachbychude.Count(),
+        //                };
+        //    //var count = (from c in items group c by c.Macd).Count();
+        //    return count;
+        //}
+
         // GET: Books
-        public ActionResult Index(int id=1)
+        public ActionResult Index()
         {
             //ViewBag.ListAllBooks = GetAllBooks();
             //ViewBag.ListBookCate = GetChudes();
             //return View();
-            
-            if (id == 1)
-            {
-                sach = context.Saches.ToList();
-            }
-            else
-            {
-                sach = context.Saches.Where(i => i.Macd == id).ToList();
-            }        
+
+
+            var sach = context.Saches.ToList();
             return View(sach);
 
 
         }
-        public ActionResult GetProductTag()
+        public ActionResult Details(int id)
         {
 
+            var model = context.Saches.Find(id);
+            ViewBag.productRelated = context.Saches.Where(i => i.Macd == model.Macd).ToList();
+            ViewBag.productUpsell = context.Saches.Where(i => i.Giakm > model.Giakm).ToList();
+            return View(model);
+        }
+        public ActionResult GetProductTag(int? id)
+        {
+            ViewBag.book = GetBookByChude(id);
             var chude = from cd in context.Chudes select cd;
             return PartialView(chude);
         }
-        public ActionResult SanPhamTheoChuDe(int id)
+        public ActionResult BookByCategory(int id)
         {
-            var sach = context.Saches.Where(i => i.Macd == id);
-            return View();
+            var sach = context.Saches.Where(i => i.Macd == id).ToList();
+            return View(sach);
 
         }
+        public ActionResult ProductCategory()
+        {
+            var items = context.Chudes.ToList();
 
+            //var count = from s in items.GroupBy(s=>s.Macd)
+            //            select new
+            //            {
+            //                Macd = s.Key,
+            //                soluong1 = s.Count(),
+            //            };
+            var arr = new int[items.Count()];
+            var j = 0;
+            foreach (var it in items)
+            {              
+                arr[j]=context.Saches.Where(i => i.Macd == it.Macd).Count();
+                j++;
+            }
+            ViewBag.countBook = arr;
+            ViewBag.category = GetChudes();
+            return PartialView();
+        }
 
   
 
--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -11,30 +11,16 @@ namespace BookStore.Controllers
     public class HomeController : Controller
     {
         private Model1 context = new Model1();
-        private List<Sach> GetNewBooks(int count)
-        {
-            return context.Saches.OrderByDescending(i => i.Ngaycapnhat).Take(count).ToList();
-        }
-        private List<Chude> GetChudes()
-        {
-            return context.Chudes.Where(i => i.Tenchude != null).ToList();
-        }
-
-
-        private List<Sach> GetAllBooks()
-        {
-            return context.Saches.ToList();
-        }
-
         public ActionResult Index()
         {
 
-            ViewBag.ListBookCate = GetChudes();
-            ViewBag.ListAllBooks = GetAllBooks();
-            var newbooks = GetNewBooks(5);
-                return View(newbooks);
+            ViewBag.ListBookCate = context.Chudes.Where(i => i.Tenchude != null).ToList(); ;
+            ViewBag.ListAllBooks = context.Saches.ToList(); ;
+            ViewBag.ListBookBestSale = context.Saches.OrderByDescending(i => i.Giakm / i.Dongia).ToList();
+            var newbooks = context.Saches.OrderByDescending(i => i.Ngaycapnhat).Take(5).ToList();           
+            return View(newbooks);
         }
-
+        
         
         
     }
