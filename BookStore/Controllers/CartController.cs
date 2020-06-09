932f18080823b3062fb2da07349d1c8e6d28e6bd
minhitmta2016 <congminh.it.mta@gmail.com>
2020-06-09 19:24:05 +0700

    SỬA LẠI GIAO DIỆN, XÓA CHỨC NĂNG LINH TINH

---
+++ BookStore/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult Checkout()
+        {
+
+            return View();
+        }
+    }
+}
